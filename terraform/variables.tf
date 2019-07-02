02b10601f52c27e5f20afea3b35a93bda990bc73
Alexey Malchenko <sinhrofasatron@gmail.com>
2019-07-02 17:01:07 +0300

    HW6 terra-1

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+
+  # Значение по умолчанию
+
+  default = "europe-west1"
+}
+
+variable public_key_path {
+  # Описание переменной
+
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable zone {
+  description = "Default zone"
+}
